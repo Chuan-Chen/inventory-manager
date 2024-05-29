06d870725b79d7997e330fd03ee024178934b486
Chuan Chen <chuan.chen402@qmail.cuny.edu>
2024-05-29 10:26:07 -0400

    minor updates

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -22,15 +22,27 @@ const startBtn = {
 
 function LandingPage(){
     return (
-        <div style = {page}>
-            <div>
-                <div>
-                    <div style = {{fontWeight: "bold", fontSize: "60px"}}>Shelfy<img height = "16px" src = {logo}></img></div>
-                    <div>The Inventory mangement service you deserve<img height = "5px" src = {logo}></img></div>
+        <div style = {{height: "100%", width: "100%", display:"grid"}}>
+            <div style = {{height: "80%", width: "80%", alignSelf: "center", justifySelf: "center", display: "grid", backgroundColor: "black"}}>
+                <div style = {{height: "95%", width: "95%", alignSelf: "center", justifySelf: "center", display: 'grid', border: '6px solid white'}}>
+                    <div style = {{height: "95%", width: "95%", alignSelf: "center", justifySelf: "center",  border: '1px solid grey'}}>
+                        <div style = {page}>
+                            <div>
+                                <div>
+                                    <div style = {{fontWeight: "bold", fontSize: "60px"}}>Shelfy<img height = "16px" src = {logo}></img></div>
+                                    <div>The Inventory mangement service you deserve<img height = "5px" src = {logo}></img></div>
+                                </div>
+                            </div>
+                            <LinkBtn link = "/app" text = "Get Started" style = {startBtn}></LinkBtn>
+                        </div>
+                    </div>
+                    
                 </div>
+           
             </div>
-            <LinkBtn link = "/app" text = "Start" style = {startBtn}></LinkBtn>
         </div>
+        
+
     )
 }
 
